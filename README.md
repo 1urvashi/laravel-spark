782a7846917a967c4534ec81a749de446571c0fb
urvi-PC\urvi <urvashidhokiya8@gmail.com>
2017-09-19 15:29:41 +0530

    ghjhjghj

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# laravel-spark
+# Spark-based Laravel installation
+
+Since `laravel/spark` is not free anymore and cannot be installed or updated with `composer`, you can use this basic
+installation of Laravel 5.2 with all Spark code integrated in it.
+
+## Releases
+
+The version numbering follows [Laravel releases](https://github.com/laravel/laravel/releases).
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ app/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use App\Teams\CanJoinTeams;
+use Laravel\Cashier\Billable;
+use Illuminate\Foundation\Auth\User as BaseUser;
+use App\Auth\TwoFactor\Authenticatable as TwoFactorAuthenticatable;
+use App\Contracts\Auth\TwoFactor\Authenticatable as TwoFactorAuthenticatableContract;
+
+class User extends BaseUser implements TwoFactorAuthenticatableContract
+{
+    use Billable, TwoFactorAuthenticatable, CanJoinTeams;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email',
+        'name',
+        'password',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'using_two_factor_auth'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'card_brand',
+        'card_last_four',
+        'extra_billing_info',
+        'password',
+        'remember_token',
+        'stripe_id',
+        'stripe_subscription',
+        'two_factor_options',
+    ];
+}
